8853ef0931d7449fb8f49d633f7f059cd1dd9146
baylovka <baylovka.16@gmail.com>
2025-02-28 12:11:41 +0200

    feat: make children is optional

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,14 +4,14 @@ import Icon from "./Icon";
 interface ButtonProps {
     iconId?: string;
     className?: string;
-    children: ReactNode;
+    children?: ReactNode;
 };
 
 export default function Button({ iconId, className, children }: ButtonProps) {
     return (
         <button className={`button ${className}`}>
             {iconId && <Icon id={iconId} width={16} height={16} />}
-            {children}
+            {children && children}
         </button>
     );
 }
